d07a275dbfb2e750afdc01d7eb6c2131648bc360
rezak9876 <rezak9876.official@gmail.com>
2023-06-29 19:37:32 +0330

    chore: Change error-monitoring config name to error-monitoring-laravel

---
+++ src/config/error-monitoring-laravel.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'token' => 'a2918bf3b6d6867d96385add0a8cc5042f35a4e6',
+    'host' => 'http://error-monitoring.test',
+];
--- src/config/error-monitoring.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'token' => 'b83f984d24a96d2185b238f878108420f9dafbed',
-];
